3e027c0740825fc986ee503d817338249320ef06
muhammadsalesfinity <muhammad@salesfinity.co>
2026-06-26 16:26:17 +0500

    show port on log

--- src/main.ts
+++ src/main.ts
@@ -47,8 +47,8 @@ async function bootstrap() {
     },
   });
   
-  await app.listen(process.env.PORT ?? 3050);
-  console.log(`Application is running on: http://localhost:${process.env.PORT ?? 3000}`);
-  console.log(`Swagger documentation: http://localhost:${process.env.PORT ?? 3000}/swagger`);
+  await app.listen(process.env.PORT ?? 3050, () => {
+		console.log(`Server is running on port ${process.env.PORT ?? 3050}`);
+	})
 }
 bootstrap();
